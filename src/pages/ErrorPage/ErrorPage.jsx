993c8ef6bb6002e677a5e07c1c2e0a2534b84648
Lakshya Sadhwani <lakshyamwork@gmail.com>
2023-10-23 15:49:16 +0530

    fixed bug

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import './ErrorPage.css'
 
 
 const ErrorPage = () => {
-
+const navigate=useNavigate();
     return (
         <div className="error-container">
             <p>Error 404</p>
             <p>No Such Page Found</p>
+            <button onClick={()=>navigate('/')}>Go Back</button>
         </div>
     );
 }
